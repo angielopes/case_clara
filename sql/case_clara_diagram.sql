219e115d7d51917b71eff687ab8fc9b54b014017
Angie <i4ngela@gmail.com>
2025-05-14 13:46:24 -0300

    Add SQL schema for user and page tables

---
+++ sql/case_clara_diagram.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `user_table` (
+    `user_id` VARCHAR(10) PRIMARY KEY,
+    `date` DATE,
+    `device` ENUM('Desktop', 'Mobile'),
+    `sex` ENUM('Female', 'Male')
+);
+
+CREATE TABLE `home_page_table` (
+    `user_id` VARCHAR(10) PRIMARY KEY,
+    `page` ENUM('home_page')
+);
+
+CREATE TABLE `payment_confirmation_table` (
+    `user_id` VARCHAR(10) PRIMARY KEY,
+    `page` ENUM('payment_confirmation_page')
+);
+
+CREATE TABLE `payment_page_table` (
+    `user_id` VARCHAR(10) PRIMARY KEY,
+    `page` ENUM('payment_page')
+);
+
+CREATE TABLE `search_page_table` (
+    `user_id` VARCHAR(10) PRIMARY KEY,
+    `page` ENUM('search_page')
+);
+
+ALTER TABLE `home_page_table` ADD FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`);
+
+ALTER TABLE `payment_confirmation_table` ADD FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`);
+
+ALTER TABLE `payment_page_table` ADD FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`);
+
+ALTER TABLE `search_page_table` ADD FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`);
