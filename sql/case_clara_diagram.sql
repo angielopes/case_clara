c169af275cd78de74f562b02d02d86fe5003f3bf
Angie <i4ngela@gmail.com>
2025-05-14 14:38:46 -0300

    Remove PRIMARY KEY constraint from user_id in page tables

--- sql/case_clara_diagram.sql
+++ sql/case_clara_diagram.sql
@@ -6,22 +6,22 @@ CREATE TABLE `user_table` (
 );
 
 CREATE TABLE `home_page_table` (
-    `user_id` VARCHAR(10) PRIMARY KEY,
+    `user_id` VARCHAR(10),
     `page` ENUM('home_page')
 );
 
 CREATE TABLE `payment_confirmation_table` (
-    `user_id` VARCHAR(10) PRIMARY KEY,
+    `user_id` VARCHAR(10),
     `page` ENUM('payment_confirmation_page')
 );
 
 CREATE TABLE `payment_page_table` (
-    `user_id` VARCHAR(10) PRIMARY KEY,
+    `user_id` VARCHAR(10),
     `page` ENUM('payment_page')
 );
 
 CREATE TABLE `search_page_table` (
-    `user_id` VARCHAR(10) PRIMARY KEY,
+    `user_id` VARCHAR(10),
     `page` ENUM('search_page')
 );
 
